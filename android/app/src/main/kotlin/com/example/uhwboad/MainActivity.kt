f95f5daf86aacf631965906949960529919fe547
Wahaj  Ashraf <deasysoftjaswahaj4husw@gmail.com>
2021-06-09 21:24:02 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/uhwboad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uhwboad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:whiteboardkit/whiteboardkit.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'White Board'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  DrawingController controller;
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Offset> _points = <Offset>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: new Container(
+        child: new GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox object = context.findRenderObject();
+              Offset _localPosition =
+              object.globalToLocal(details.globalPosition);
+              _points = new List.from(_points)
+                ..add(_localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: new CustomPaint(
+            painter: new Signature(points: _points), size: Size.infinite,),
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.clear),
+        onPressed: () => _points.clear(),
+      ),
+    );
+  }
+}
+
+class Signature extends CustomPainter{
+
+  List<Offset> points;
+
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    Paint paint = new Paint()
+    ..color = Colors.black
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 01;
+
+
+    for(int i = 0;i<points.length-1 ; i++){
+      if(points[i] !=null && points[i+1] != null)
+        canvas.drawLine(points[i], points[i+1], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+  }
+
+
+
